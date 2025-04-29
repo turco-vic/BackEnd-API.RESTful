565f534fc49a47a0285a83b05a84b4aced9c41f0
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-04-29 20:05:58 -0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const heroRoutes = require("./src/routes/heroRoutes");
+const publisherRoutes = require("./src/routes/publisherRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/heros", heroRoutes);
+app.use("/api/publishers", publisherRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
